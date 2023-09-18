7cec503fba78249349f450e2b37f04c6cecfb12f
Jayjay-Shou <99698408+Jayjay-Shou@users.noreply.github.com>
2023-09-18 17:15:47 +1000

    Update streamlit_app.py
    remove resoponse

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Grapes'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
-
 streamlit.dataframe(fruits_to_show)
-
 
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + "kiwi")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
